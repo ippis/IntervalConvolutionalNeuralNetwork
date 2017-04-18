6df4d9586451031ac1dbaf95abb4df292deec024
Lucas Mendes Tortelli <lmtortelli@hotmail.com>
2017-04-18 11:12:46 -0300

    Atualização script

--- script.py
+++ script.py
@@ -42,11 +42,11 @@ filter.append(initInterval(aux,0.00000000001))
 for i in xrange(1):
         b = ConvNetInterval()
         b.load(True,1)
-        b.evaluateNetConv(filter,1,0.6,"arq1.txt",3,1,0,1)
+        b.evaluateNetConv(filter,1,0.6,"arq1.txt","8Viz",3,1,0,1)
         b = None
 
 for i in xrange(1):
         b = ConvNetInterval()
         b.load(False,1)
-        b.evaluateNetConv(filter,1,0.6,"arq1.txt",3,1,0,1)
+        b.evaluateNetConv(filter,1,0.6,"arq1.txt","4Viz",3,1,0,1)
         b = None
